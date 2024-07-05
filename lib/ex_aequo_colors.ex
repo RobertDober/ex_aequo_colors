8dd443730c1e2388ba1c26e2756c62dc3a5d45d8
RobertDober <robert.dober@gmail.com>
2024-07-05 19:30:16 +0200

    Initial

---
+++ lib/ex_aequo_colors.ex
@@ -0,0 +1,18 @@
+defmodule ExAequoColors do
+  @moduledoc """
+  Documentation for `ExAequoColors`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAequoColors.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aequo_colors_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAequoColorsTest do
+  use ExUnit.Case
+  doctest ExAequoColors
+
+  test "greets the world" do
+    assert ExAequoColors.hello() == :world
+  end
+end
