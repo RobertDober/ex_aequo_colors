ccd12006e782c16b4a8df537d15be23f21fc0860
RobertDober <robert.dober@gmail.com>
2025-06-24 16:08:08 +0200

    Release v0.1.4 for Elixir 1.19
        - Better Test Coverage
        - Got rid of warning
        - upgraded deps to ex_aequo_base 0.1.6, extractly 0.6.1 and minipeg
          0.7.6

--- test/ex_aequo_colors_test.exs
+++ test/ex_aequo_colors_test.exs
@@ -2,10 +2,10 @@ defmodule ExAequoColorsTest do
   use ExUnit.Case
   doctest ExAequoColors, import: true
 
-  @version_rgx ~r/\A \d+ \. \d+ \. \d+ /x
+  def version_rgx, do: ~r/\A \d+ \. \d+ \. \d+ /x
 
   test "version is defined and well formed" do
-    assert Regex.match?(@version_rgx, ExAequoColors.version())
+    assert Regex.match?(version_rgx(), ExAequoColors.version())
   end
 end
 
